754b9950d81d7c9cd85bec18542574446fdcafc1
Martin <martin.palfroy@arpilabe.com>
2023-06-10 21:18:41 +0200

    useRef instead of useState

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import PostCollection from '../components/postCollection';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Badge from 'react-bootstrap/Badge';
 
 const Home = () => {
 
-  const [posts, setPosts] = useState([]);
+  const allPosts = useRef([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
 
   const fetchComments = async () => {
@@ -26,7 +26,8 @@ const Home = () => {
       //comments count by post id
       posts.forEach(post => post.commentsCount = 
         comments.filter(comment => comment.postId === post.id).length);
-      setPosts(posts);
+
+      allPosts.current = posts;
       setFilteredPosts(posts);
     }
 
@@ -38,10 +39,10 @@ const Home = () => {
     <Container>
       <InputGroup className="mb-3 mt-3">
         <Form.Control placeholder="Rechercher..." onChange={(event) => 
-            setFilteredPosts(posts.filter(post => post.body.replace(/(\r\n|\n|\r)/gm, " ").includes(event.target.value)))} />
+            setFilteredPosts(allPosts.current.filter(post => post.body.replace(/(\r\n|\n|\r)/gm, " ").includes(event.target.value)))} />
       </InputGroup>
       <h6>
-        <Badge bg="secondary">{filteredPosts.length} </Badge> posts sur <Badge bg="secondary">{posts.length} </Badge>
+        <Badge bg="secondary">{filteredPosts.length} </Badge> posts sur <Badge bg="secondary">{allPosts.current.length} </Badge>
       </h6>
       <PostCollection elements={filteredPosts} />
     </Container>
